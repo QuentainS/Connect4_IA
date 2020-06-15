10fb1548df8f1ee0fb13397cfe1b26d139a23287
Schietecatte Quentain <quentain.s@gmail.com>
2020-06-15 23:37:25 +0200

    [add] quick bot docstring

--- game/bot.py
+++ game/bot.py
@@ -1,4 +1,7 @@
 def compute_move(player_number, history):
+    """ Return the column number (typically from 0 to 5) where to put the disc """
+    # player_number = 1 or 2
+    # history is an array list containing each state of the game (for each turn)
 
     # This is your bot
     print("Your move : ")
