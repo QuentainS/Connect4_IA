7692a826e26c84096b40c793edf355af5c06992c
tsepton <tsepton@gmail.com>
2020-06-16 12:46:59 +0200

    [add] Add UI for board game

--- game/board/src/index.js
+++ game/board/src/index.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom"
 import './styles.css'
 
 function App() {
   return (
     <>
-      <div>
+      <div id='title'>
         <b>Contact 4 board game</b>
       </div >
-      <div>
+      <div id='board-container'>
         <Board />
       </div>
     </>
@@ -16,7 +16,32 @@ function App() {
 }
 
 function Board() {
-  return <div>Board</div>
+  // Board representation (6x7) :
+  // List of (6) lists, each inner list is equivalent to a row, 
+  // containing 7 div elements.
+  // Each one of these represents the intersection of a column and a row 
+  const [board, setBoard] = useState([])
+
+  useEffect(() => {
+    let rows = []
+    for (let row = 0; row < 6; row++) {
+      let actualRow = []
+      for (let col = 0; col < 7; col++) {
+        actualRow.push(<div className={`box row-${row} col-${col}`}></div>)
+      }
+      rows.push(actualRow)
+    }
+    setBoard([...rows])
+    console.log(board)
+  }, [])
+
+  return (
+    <div className='board'>
+    {
+      board.map(row => row.map(jsx => jsx))
+    }
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
